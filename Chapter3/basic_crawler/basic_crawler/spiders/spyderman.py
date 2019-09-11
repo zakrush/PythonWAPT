a44acf1ed7d2141bd46f0477857421336f821e66
zakrush <zakrush@yandex.ru>
2019-09-11 23:25:17 +0300

    add reqursive parsing

--- Chapter3/basic_crawler/basic_crawler/spiders/spyderman.py
+++ Chapter3/basic_crawler/basic_crawler/spiders/spyderman.py
@@ -2,6 +2,7 @@ from scrapy.spiders import BaseSpider
 from scrapy.selector import Selector
 from basic_crawler.items import BasicCrawlerItem
 from scrapy.http import Request
+import re
 
 
 class MySpider(BaseSpider):
@@ -18,3 +19,18 @@ class MySpider(BaseSpider):
             book = BasicCrawlerItem()
             book["title"] = title
             yield book
+
+        visited_links = []
+        links = hxs.xpath('//a/@href').extract()
+        link_validator = re.compile(
+            "^(?:http|https):\/\/(?:[\w\.\-\+]+:{0,1}[\w\.\-\+]*@)?(?:[a-z0-9\-\.]+)(?::[0-9]+)?(?:\/|\/(?:[\w#!:\.\?\+="
+            "&amp;%@!\-\/\(\)]+)|\?(?:[\w#!:\.\?\+=&amp;%@!\-\/\(\)]+))?$")
+
+        for link in links:
+            if link_validator.match(link) and link not in visited_links:
+                visited_links.append(link)
+                yield Request(link, self.parse)
+            else:
+                full_url = response.urljoin(link)
+                visited_links.append(full_url)
+                yield Request(full_url, self.parse)
