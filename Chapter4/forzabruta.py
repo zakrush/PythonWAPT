3b9a7a83f5f8f231e09d597d952e9e61a1a344d9
zakrush <zakrush@yandex.ru>
2019-09-17 14:47:29 +0300

    add md5 redirects and timing

--- Chapter4/forzabruta.py
+++ Chapter4/forzabruta.py
@@ -2,13 +2,17 @@ import requests
 from threading import Thread
 import sys
 import getopt
-import re
 from termcolor import colored
+
+import hashlib
+import time
+
 
 def banner():
     print("\n********************")
     print('*  ForzaBruta 0.2  *')
     print("********************")
+
 
 def usage():
     print("Usage:")
@@ -18,12 +22,13 @@ def usage():
     print("          -f: dictionary file \n")
     print("example: forzabruta.py -w http://targetsite.com/FUZZ -t 5 -f common.txt -c 404\n")
 
+
 class request_performer(Thread):
-    def __init__(self,word,url,hidecode):
+    def __init__(self, word, url, hidecode):
         Thread.__init__(self)
         try:
             self.word = word.split("\n")[0]
-            self.urly = url.replace('FUZZ',self.word)
+            self.urly = url.replace('FUZZ', self.word)
             self.url = self.urly
             self.hidecode = hidecode
         except Exception as e:
@@ -31,34 +36,43 @@ class request_performer(Thread):
 
     def run(self):
         try:
+
+            start = time.time()
             r = requests.get(self.url)
+            endtime = time.time()
+            totaltime = str(endtime - start)
             lines = str(len(r.text.splitlines()))
             chars = str(len(r.text))
             words = str(len(r.text.split()))
-            #words = str(len(re.findall("\S+", r.text)))
+            # words = str(len(re.findall("\S+", r.text)))
             code = str(r.status_code)
+            hash = hashlib.md5(r.text.encode()).hexdigest()
+
+            if r.history != []:
+                code = str(r.history[0].status_code)
 
             if self.hidecode != code:
                 if '200' <= code < '300':
-                    print(colored(code, 'green') + "   \t\t" + chars +
-                          " \t\t" + words + " \t\t " + lines + "\t" + self.url + "\t\t  ")
+                    print(totaltime + "\t\t" + colored(code, "green") + "\t" + chars +
+                          "\t" + words + "\t " + lines + "\t" + hash[:-7:-1] + "\t" + self.url + "\t\t  ")
                 elif '400' <= code < '500':
-                    print(colored(code,'red') + "   \t\t" + chars +
-                          " \t\t" + words + " \t\t " + lines + "\t" + self.url + "\t\t  ")
+                    print(totaltime + "\t\t" + colored(code, "red") + "\t" + chars +
+                          "\t" + words + "\t " + lines + "\t" + hash[:-7:-1] + "\t" + self.url + "\t\t  ")
                 elif '300' <= code < '400':
-                    print(colored(code,'blue') + "   \t\t" + chars +
-                          " \t\t" + words + " \t\t " + lines + "\t" + self.url + "\t\t  ")
+                    print(totaltime + "\t\t" + colored(code, "blue") + "\t" + chars +
+                          "\t" + words + "\t " + lines + "\t" + hash[:-7:-1] + "\t" + self.url + "\t\t  ")
                 else:
-                    print(colored(code,'yellow') + "   \t\t" + chars +
-                          " \t\t" + words + " \t\t " + lines + "\t" + self.url + "\t\t  ")
+                    print(totaltime + "\t\t" + colored(code, "yellow") + "\t" + chars +
+                          "\t" + words + "\t " + lines + "\t" + hash[:-7:-1] + "\t" + self.url + "\t\t  ")
 
             else:
                 pass
 
             #            print(self.url + "-" + str(r.status_code))
-            i[0] = i[0]-1 #удаляем одну нить из счетчика
+            i[0] = i[0] - 1  # удаляем одну нить из счетчика
         except Exception as e:
             print(e)
+
 
 def start(argv):
     banner()
@@ -68,10 +82,10 @@ def start(argv):
     try:
         opts, args = getopt.getopt(argv, "w:f:t:c:")
     except getopt.GetoptError:
-        print ("Error en arguments")
+        print("Error en arguments")
         sys.exit()
     hidecode = 000
-    for opt,arg in opts:
+    for opt, arg in opts:
         if opt == '-w':
             url = arg
         elif opt == '-f':
@@ -82,29 +96,32 @@ def start(argv):
             hidecode = arg
 
     try:
-        f = open(dict,"r")
+        f = open(dict, "r")
         words = f.readlines()
     except:
         print("Failed opening file: " + dict)
         sys.exit()
 
-    launcher_thread(words,threads,url,hidecode)
+    launcher_thread(words, threads, url, hidecode)
 
-def launcher_thread(names,th,url,hidecode):
+
+def launcher_thread(names, th, url, hidecode):
     global i
     i = []
     resultlist = []
     i.append(0)
-    print("-------------------------------------------------------------------------------------------------------------")
-    print("Code\t\tchars\t\twords\t\tlines\t\tURL")
-    print("-------------------------------------------------------------------------------------------------------------")
+    print(
+        "-------------------------------------------------------------------------------------------------------------")
+    print("Time\t\t\t\tCode\tchars\twords\tlines\tMD5\t\t\tURL")
+    print(
+        "-------------------------------------------------------------------------------------------------------------")
 
     while len(names):
         try:
             if i[0] < th:
                 n = names.pop(0)
                 i[0] = i[0] + 1
-                thread = request_performer(n,url,hidecode)
+                thread = request_performer(n, url, hidecode)
                 thread.start()
 
         except KeyboardInterrupt:
@@ -113,9 +130,9 @@ def launcher_thread(names,th,url,hidecode):
         thread.join()
     return
 
+
 if __name__ == "__main__":
     try:
         start(sys.argv[1:])
     except KeyboardInterrupt:
         print("ForzaBruta interrupted by user, killing all threads..!!")
-
