e5deb48a99baebcca3f151ef4ac1fe926e658763
zakrush <zakrush@yandex.ru>
2019-10-20 22:58:06 +0300

    working query GET modify

--- Chapter7/mitm.py
+++ Chapter7/mitm.py
@@ -2,14 +2,16 @@ import sys
 global history
 history = []
 
-def request(context, flow):
+def request(flow):
 
     url = flow.request.url
-    if url not in history:
-        f = open('httplogs.txt', 'a+')
-        f.write(url +'/n')
-        history.append(url)
+    try:
+        f = open('./httplogs.txt', 'a+')
+        if url not in history:
+            f.write(url +'\n')
+            history.append(url)
+        else:
+            pass
+    finally:
         f.close() #после проверки работоспособности попробовать открытие вынести за условие
-        #также добавить обработку ошибки неоткрытия и не закрытия файла (стандартная конструкция)
-    else:
-        pass
+            #также добавить обработку ошибки неоткрытия и не закрытия файла (стандартная конструкция)
--- Chapter7/mitm2.py
+++ Chapter7/mitm2.py
@@ -1,7 +1,8 @@
 import sys
 
-def request(context, flow):
-    q = flow.request.get_query() #после обновления посмотреть в дебаге, что хранится в данной переменной
+def request(flow):
+    q = flow.request.query #после обновления посмотреть в дебаге, что хранится в данной переменной
     if q:
-        q["isadmin"] = ["True"]
-        flow.request.set_query(q)
+        q["id"] = "10"
+        q["isadmin"] = "True"
+       # flow.request.query(q)
